c091e6e55aaa59c687f23e428d1d72529e138c2f
PrithiviPM2580 <prithivipemimagar>
2025-10-23 19:06:15 +0545

    feat: implement basic LLM call, output parser, prompt template, and tools for weather and news retrieval

--- index.ts
+++
@@ -1,9 +0,0 @@
-//Phase 1: Basic LLM Call
-// import "./src/langchain/phase1/basic_llm_call";
-// import "./src/langchain/phase1/prompt_template";
-// import "./src/langchain/phase1/output_parser";
-// import "./src/langchain/phase1/chains";
-
-//Phase 2: Tools and Agent
-// import "./src/langchain/phase2-Tools_And_Agent/connect_tool_to_agent";
-import "./src/langchain/phase2-Tools_And_Agent/add_multiple_tools_to_agent";
