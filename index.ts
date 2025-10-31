d0083b158d60ea7322b268b0c670189fe5617bcb
PrithiviPM2580 <prithivipemimagar>
2025-11-01 03:39:00 +0545

    feat: implement state graph workflow with memory saving and state history logging

--- index.ts
+++ index.ts
@@ -1,7 +1,38 @@
-// import "./src/langgraph-document/workflow-agent/prompt_chaining";
-// import "./src/langgraph-document/workflow-agent/parallelization_workflow";
-// import "./src/langgraph-document/workflow-agent/routing_workflow";
-// import "./src/langgraph-document/workflow-agent/orchestrator_worker_workflow";
-// import "./src/project/blog_agent";
-// import "./src/langgraph-document/workflow-agent/evaluator-optimizer_workflow";
-import "./src/langgraph-document/workflow-agent/agents_workflow";
+import { StateGraph, START, END, MemorySaver } from "@langchain/langgraph";
+import { registry } from "@langchain/langgraph/zod";
+import * as z from "zod";
+
+const State = z.object({
+  foo: z.string(),
+  bar: z.array(z.string()).register(registry, {
+    reducer: {
+      fn: (x, y) => x.concat(y),
+    },
+    default: () => [] as string[],
+  }),
+});
+
+const workflow = new StateGraph(State)
+  .addNode("nodeA", (state) => {
+    return { foo: "a", bar: ["a"] };
+  })
+  .addNode("nodeB", (state) => {
+    return { foo: "b", bar: ["b"] };
+  })
+  .addEdge(START, "nodeA")
+  .addEdge("nodeA", "nodeB")
+  .addEdge("nodeB", END);
+
+const checkpointer = new MemorySaver();
+const graph = workflow.compile({ checkpointer });
+
+const config = { configurable: { thread_id: "1" } };
+(async () => {
+  const response = await graph.invoke({ foo: "" }, config);
+  const state = await graph.getState(config);
+  console.log("Response: ", response);
+  console.log("Get State: ", state);
+  for await (const step of graph.getStateHistory(config)) {
+    console.log(step);
+  }
+})();
